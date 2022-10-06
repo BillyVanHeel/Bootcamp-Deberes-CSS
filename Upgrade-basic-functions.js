1e2d67d5816d3e8c28ee060d46db2c5df84c5bf7
Guillermo Chacón Poveda <guillermo.chacon@bootcamp-upgrade.com>
2022-10-06 12:14:46 +0200

    iteration#5

--- Upgrade-basic-functions.js
+++ Upgrade-basic-functions.js
@@ -32,10 +32,44 @@ const numbers=[1,2,3,5,45,37,58];
 
 function sumAll(array){
     let sum=0;
-    for (let i = 0; i < array.length; i++){
-        sum+=array[i];
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
         
-    }        
-    return sum;
     }
-console.log(sumAll (numbers));
+    return sum;
+}
+
+console.log(sumAll(numbers));
+
+//Iteración 4
+
+const factors = [12, 21, 38, 5, 45, 37, 6];
+
+function average(array){
+    let sum=0;
+    let result=0;
+ for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    result=(sum+=array[i])/array.length;
+ }
+ return result
+}
+console.log(average(factors));
+
+//Iteración 5
+
+const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
+function averageWord(array){
+    let sum=0;
+    for (let i = 0; i < array.length; i++) {
+        if(typeof array[i]=='string'){
+            sum+=array[i].length;
+        }
+        else if (typeof array[i]=='number'){
+            sum+=array[i];
+        }
+        
+    }
+    return sum;
+}
+console.log(averageWord(mixedElements));
