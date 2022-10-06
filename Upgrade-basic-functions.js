135c3bbd69157881d6faf6593489618ae90373ce
Guillermo Chacón Poveda <guillermo.chacon@bootcamp-upgrade.com>
2022-10-06 10:52:51 +0200

    iteration#3

--- Upgrade-basic-functions.js
+++ Upgrade-basic-functions.js
@@ -25,3 +25,17 @@ for(const element of array){
 }
 
 console.log(findLongestWord(avengers));
+
+//Iteration 3
+
+const numbers=[1,2,3,5,45,37,58];
+
+function sumAll(array){
+    let sum=0;
+    for (let i = 0; i < array.length; i++){
+        sum+=array[i];
+        
+    }        
+    return sum;
+    }
+console.log(sumAll (numbers));
