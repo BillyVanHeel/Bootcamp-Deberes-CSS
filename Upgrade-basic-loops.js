5003d990662d2269d813fe10bb984d521e1d6096
Guillermo Chacón Poveda <guillermo.chacon@bootcamp-upgrade.com>
2022-10-08 23:31:32 +0200

    iteration#7

--- Upgrade-basic-loops.js
+++ Upgrade-basic-loops.js
@@ -92,3 +92,21 @@ for (let i = 0; i < placesToVisit.length; i++) {
             console.log(toy);
         }   
     }
+
+    console.log('\n'+'---Iteración 7---'+'\n');
+
+const popularToys = [];
+const regularToys = [
+	{id: 5, name: 'Buzz MyYear', sellCount: 10}, 
+	{id: 11, name: 'Action Woman', sellCount: 24}, 
+	{id: 23, name: 'Barbie Man', sellCount: 15}, 
+	{id: 40, name: 'El gato con Guantes', sellCount: 8},
+	{id: 40, name: 'El gato felix', sellCount: 35}
+]
+
+    for (const stock of regularToys) {
+        if (stock.sellCount > 15) {
+            popularToys.push(stock);
+        }
+    }
+    console.log(popularToys);
