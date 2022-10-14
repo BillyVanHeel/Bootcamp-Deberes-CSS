968a23ea4412a5721b14f20137a8a796f53fe3e0
Guillermo Chacón Poveda <guillermo.chacon@bootcamp-upgrade.com>
2022-10-14 11:56:55 +0200

    FEAT: Upgrade-asynchronous, Iteration#3

--- Upgrade-asynchronous/Upgrade-asynchronous.js
+++ Upgrade-asynchronous/Upgrade-asynchronous.js
@@ -1,13 +1,43 @@
 //Iteración 1
-fetch('https://api.agify.io?name=michael')
-.then((michael) => michael.json())
-.then ((michael) => {(console.log(michael));});
+fetch("https://api.agify.io?name=michael")
+  .then((michael) => michael.json())
+  .then((michael) => {
+    console.log(michael);
+  });
 
 //Iteración 2
-const baseUrl = 'https://api.nationalize.io/?name=';
- function seek (){
-    fetch(baseUrl);
- }
+const baseUrl = "https://api.nationalize.io/?name=";
+const petitionButton = document.querySelector("button");
+const petitionInput = document.querySelector("input");
+const namePetition = petitionInput.value;
 
- let seekingButton = document.querySelector('button');
- seekingButton.addEventListener('click', seek);
+const performPetition = () => {
+  const namePetition = petitionInput.value;
+  fetch(baseUrl + namePetition)
+    .then((response) => response.json())
+    .then((response) => displayPetition(response));
+};
+
+petitionButton.addEventListener("click", performPetition);
+
+//Iteración 3
+const displayPetition = (response) => {
+  const petitionOutput = document.createElement("p");
+  petitionOutput.textContent = `El nombre ${response.name} `; 
+  
+  for (let i = 0; i < response.country.length; i++) {
+    let land = response.country[i];
+        if(response.country.indexOf(land)=== 0){
+            petitionOutput.textContent += `tiene un ${(response.country[i].probability*100).toFixed(1)} porciento de ser de ${response.country[i].country_id}` }
+        else if(response.country.indexOf(land) !== response.country.length-1){
+            petitionOutput.textContent += `, un ${(response.country[i].probability*100).toFixed(1)} porciento de ser de ${response.country[i].country_id}`}
+        else{
+            petitionOutput.textContent += ` y un ${(response.country[i].probability*100).toFixed(1)} porciento de ser de ${response.country[i].country_id}`}
+    };
+
+  document.body.appendChild(petitionOutput);
+  console.log(response);
+}
+
+//Iteración 4
+
